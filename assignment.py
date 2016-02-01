8be161fe72c535c3dd788c36890b7fc0e844222f
cmis <cmis@cmis-hs17.(none)>
2016-02-01 13:33:30 +0700

    a bit left

--- assignment.py
+++ assignment.py
@@ -1,8 +1,19 @@
-myname = "IsaiahKwong"
-myage = 18 + (12+29) / 365.0
-myheight = "1.74m"
+myname = "Isaiah Kwong"
+myage = 18 + (12+29+30) / 365.0
+myheightinm = 1.74
 sq1side = 3
 lengthrec = 10
 heightrec = 20
 myageinmonth = myage * 12
-print myageinmonth
+lifeexpectancy = 82.44
+mydaysleft = lifeexpectancy - myage
+mtoft = 3.28084
+myheightinft = myheightinm * mtoft
+averageheight = 1.70
+heightdifferent = myheightinm - averageheight
+areaofsq = sq1side ** 2
+halfvolume = (lengthrec ** 2 * heightrec) / 2.0
+oneovernine = (halfvolume / (heightrec/2))/9
+print "Hello world, my name is ", myname , "and I am " , myage , "years old. Compare to average age in Hong Kong, I still have", mydaysleft , " years left to live. I am ", myheightinm , "M which means I am ", myheightinft , "ft tall. I want to be at least 6 foot tall tho. T^T" 
+
+
