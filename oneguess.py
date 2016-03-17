fa3e32199fe045e1f9e032e3ff655dd15e6c0bef
cmis <cmis@cmis-hs07.(none)>
2016-03-17 08:39:27 +0700

    done

--- oneguess.py
+++ oneguess.py
@@ -5,24 +5,32 @@ import random
 #input
 
 def getno():
-	minimumno = raw_input("What is the minimum number? ")
-	maximumno = raw_input("What is the maxium number? ")
-	theguess = raw_input("What do you think it is? ")
-	theno = random.randint(minimumno, maximumno)
-	return output()
+    minimumno = int(raw_input("What is the minimum number? "))
+    maximumno = int(raw_input("What is the maxium number? "))
+    print """I'm thinking of a number from {} to {}.""".format(minimumno, maximumno)
+    theguess = int(raw_input("What do you think it is? "))
+    theno = int(random.randint(minimumno, maximumno))
+    return calculation(theguess, theno)
 
 #process
 
-def calculation():
-    if minimumno > theno:
-        return True
-    elif minimumno < theno:
+def calculation(theguess, theno):
+    thedifference = int(abs(theguess - theno))
+    if theguess > theno:
+        underorover = """That's over by {}""".format(thedifference)
+    elif theguess < theno:
+        underorover = """That's under by {}""".format(thedifference)
+    else:
+        underorover = "That's correct! You must be a psychic!!"
+    return output(theno, theguess, underorover)
     
 #output
 
-def output():
+def output(theno, theguess, underorover):
     print"""
     The target was {}.
     Your guess was {}.
-    That's {} by {}.
-    """.format(theno, theguess, underorover, thedifference)
+    {}
+    """.format(theno, theguess, underorover)
+
+getno()
