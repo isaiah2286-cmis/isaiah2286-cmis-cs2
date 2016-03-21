a17df687f89d8a4d64ba3738677a93825e6d4c24
cmis <cmis@cmis-hs12.(none)>
2016-03-21 10:55:45 +0700

    cool game

--- simple_program.py
+++ simple_program.py
@@ -1,17 +1,7 @@
 
-#output
-def output(un, a, b, c):
-	out = """Welcome {}, thank you for choosing our company. Enjoy.
-This free trail will allow you to find things about yourself...
-You are around {} days old.
-Your are {} cm away from being an average NBA player.
-Your are 1/{} of earth's mass.
-""" .format(un, a, b, c)
-
-	return out
 
 #input
-def main():
+def enter():
 	
     fn = raw_input("First Name: ")
     ln = raw_input("Last Name: ")
@@ -22,11 +12,22 @@ def main():
     w = raw_input("Your weight in kilogram: ")
     e = raw_input("Email address: ")
     pa = raw_input("Password: ")
-    a = int(age) * 365
-    b = 180 - int(h)
-    c = int(w) / 5.972 * (10**24)
-    out = output(un, a, b, c)
+    return main(age, h, w, un)
+#processing
+def main(age, h, w, un):    
+    ageinday = int(age) * 365
+    nbaheight = 200 - int(h)
+    earthtoyou = int(w) / 5.972 * (10**24)
+    return output(un, ageinday, nbaheight, earthtoyou)
 	
-    print out
+#output
+def output(un, ageinday, nbaheight, earthtoyou):
+	print """Welcome {}, thank you for choosing our company. Enjoy.
+This free trail will allow you to find things about yourself...
+You are around {} days old.
+Your are {} cm away from being an average NBA player.
+Your are 1/{} of earth's mass.
+""" .format(un, ageinday, nbaheight, earthtoyou)
+   
 
-main()
+enter()
