624b5f7e8ace06ef3bf2b670304d5147affc071d
cmis <cmis@cmis-hs11.(none)>
2016-04-26 08:40:56 +0700

    rip my life

---
+++ rerecursion.py
@@ -0,0 +1,9 @@
+def enterno():
+	bottom = int(raw_input("Enter the lowest number: "))
+	top = int(raw_input("Enter the highest number: "))
+def countupbetweenno(bottom, top):
+	if n == top:
+        print "BOOM!!!!!"
+    else:
+	print n
+        countup(n + 1) 
