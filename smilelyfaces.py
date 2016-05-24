62772c065bdce35bf3945e08e4bc780f79690bcf
cmis <cmis@cmis-hs15.(none)>
2016-05-24 13:34:02 +0700

    borin class

---
+++ smilelyfaces.py
@@ -0,0 +1,20 @@
+def down(x):
+	while x >= 0:
+		print x
+		x -= 1
+def up(x):
+	while x <= 100:
+		print x
+		x +=1
+#up(0)
+#down(100)
+
+def count(x):
+	while x >= 0:
+		print x
+		x -= 1
+	while x <= 0:
+		print x
+		x += 1
+	
+count(10)
